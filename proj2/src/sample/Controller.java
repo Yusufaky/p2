d10d2059200c012e1d06442f3df0bdf559a41415
Yusufaky <44864683+Yusufaky@users.noreply.github.com>
2021-03-03 15:18:18 +0000

    2 butoes

--- proj2/src/sample/Controller.java
+++ proj2/src/sample/Controller.java
@@ -1,4 +1,14 @@
 package sample;
 
+import javax.swing.*;
+import javafx.event.ActionEvent;
+
 public class Controller {
+    public  void pressButton(ActionEvent event){
+        System.out.println("Olá Mundo");
+    }
+    public  void pressButton2(ActionEvent event){
+        System.out.println("Olá Mundo");
+    }
 }
+
